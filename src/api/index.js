4b9909f4b2d10778eb294d75544c48f18160b9c4
Kamila W <kamila.wilczynska92@gmail.com>
2024-06-27 12:47:36 +0100

    add new endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,2 @@
-export const API_ENDPOINT = "http://localhost:3000";
+// export const API_ENDPOINT = "http://localhost:3000";
+export const API_ENDPOINT = "https://books-app-backend-2.onrender.com/";
