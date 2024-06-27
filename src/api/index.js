cb0f16113f50998088c6af3d27ad4bd81c767e54
Kamila W <kamila.wilczynska92@gmail.com>
2024-06-27 13:18:01 +0100

    correct API endpoint URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
 // export const API_ENDPOINT = "http://localhost:3000";
-export const API_ENDPOINT = "https://books-app-backend-2.onrender.com/";
+export const API_ENDPOINT = "https://books-app-backend-2.onrender.com";
